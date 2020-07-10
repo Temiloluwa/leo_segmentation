659f67c90fe25d8519978d9e5e1a6bf91fd8921f
Istiaque Mannafee Shaikat <mannafee@gmail.com>
2020-07-10 22:03:17 +0600

    small changes

--- leo_segmentation/run.py
+++ leo_segmentation/run.py
@@ -12,24 +12,27 @@ args = parser.parse_args()
 dataset = args.dataset[0]
 model_path='' 
 def train_model(config):
-    device = torch.device("cuda")
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() and config.use_gpu else "cpu")
+    Leo=LEO().to(device)
     metatrain_dataloader = Datagenerator(dataset, config, data_type="meta_train")
     episodes = config.hyperparameters.episodes
     episodes_completed=0
     train_stats_store = TrainingStats() # to store training statistics
-    optimizer = optim.SGD(LEO.parameters(), lr=0.1, momentum=0.9)
+    optimizer = optim.SGD(Leo.parameters(), lr=0.1, momentum=0.9)
+
     if load_model == True:
-        LEO,optimizer,episodes_completed=load_model(LEO,optimizer,model_path)
+        Leo,optimizer,episodes_completed=load_model(Leo,optimizer,model_path)
     
     for i in range(episodes-episodes_completed):
-        LEO.train()
+        Leo.train()
         tr_data, tr_data_masks, val_data, val_masks = metatrain_dataloader.get_batch_data()
         print("tr_data shape: {},tr_data_masks shape: {}, val_data shape: {},val_masks shape: {}".\
             format(tr_data.size(), tr_data_masks.size(), val_data.size(), val_masks.size()))
     #for loop for batch data in metatrain_dataloader
-        loss,optimizer=optimize_model(LEO,batch_tr_data,batch_tr_data_masks,optimizer)
+        loss,optimizer=optimize_model(Leo,batch_tr_data,batch_tr_data_masks,optimizer)
         if i % 1000 == 0: #saving model for every 1000 epoch
-            save_model(LEO,optimizer,model_path,i,loss)
+            save_model(Leo,optimizer,model_path,i,loss)
 
 
 def predict_model(config):
