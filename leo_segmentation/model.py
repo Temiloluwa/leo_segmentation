23072c3a45249de6141f038866dec984d3b7931f
GopikaSudhakaran <45491358+GopikaSudhakaran@users.noreply.github.com>
2020-07-11 18:18:26 +0200

    Work in progress LEO until forward decoder inside LEO is functional

--- leo_segmentation/model.py
+++ leo_segmentation/model.py
@@ -3,9 +3,12 @@
 import torch
 from torch import nn
 from torch.autograd import variable
+import numpy as np
+from torch.distributions import Normal
+
 
 class _EncoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout=False):
+    def __init__(self, in_channels, out_channels,  dropout=False):
         super(_EncoderBlock, self).__init__()
         layers = [
             nn.Conv2d(in_channels, out_channels, kernel_size=2),
@@ -37,6 +40,26 @@ class _DecoderBlock(nn.Module):
 
     def forward(self, x):
         return self.decode(x)
+
+class RelationNetwork(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout=False):
+        super(RelationNetwork, self).__init__()
+        layers = [  nn.Conv2d(in_channels, out_channels, kernel_size=2, padding=1),
+                    nn.BatchNorm2d(out_channels, momentum=1, affine=True),
+                    nn.ReLU(inplace=True),
+                    nn.Conv2d(out_channels, in_channels, kernel_size=2, padding=1),
+                    nn.BatchNorm2d(in_channels, momentum=1, affine=True),
+                    nn.ReLU(inplace=True)]
+        if dropout:
+            layers.append(nn.Dropout())
+        self.relation = nn.Sequential(*layers)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+
+    def forward(self, concat_features):
+        out = self.relation(concat_features)
+        out = self.upsample(out)
+        return out
 
 class LEO(nn.Module):
     def __init__(self, config):
@@ -76,16 +99,73 @@ class LEO(nn.Module):
         return dec1
 
     def forward_encoder(self, data):
-        latent = variable(self.encoder(data), requires_grad=True)
-        # add relation network logic
-        # add sampler
-        return latent
+
+        latent = self.encoder(data)
+        latent = latent.clone().detach().requires_grad_(True)
+        relation_network_outputs = self.relation_network(latent)
+        latent_dist_params = self.average_codes_per_class(relation_network_outputs)
+        latents, kl = self.possibly_sample(latent_dist_params)
+        return latents, kl
 
     def forward_decoder(self, data, latents):
         weights_dist_params = self.decoder(latents)
+        dim_list = list(latents.size())
+        fan_in = dim_list[1]*dim_list[2]*dim_list[3]
+        fan_out = self.config["data_type"]["train"]["num_classes"]
+        stddev_offset = np.sqrt(2. / (fan_out + fan_in))
+        classifier_weights, _ = self.possibly_sample(weights_dist_params,
+                                                     stddev_offset=stddev_offset)
         # add sampler - logic returns classifier weights
         # add inner loss
         return 'tr_loss', 'classifier_weights'
+
+    def relation_network(self, latents):
+        total_num_examples = self.config["data_type"]["train"]["num_classes"] * self.config["data_type"]["train"]["n_train_per_class"]
+        left = latents.unsqueeze(1).repeat(1, total_num_examples, 1, 1, 1)
+        right = latents.unsqueeze(0).repeat(total_num_examples, 1, 1, 1, 1)
+        concat_codes = torch.cat((left, right), dim = 2)
+        dim_list1 = list(concat_codes.size())
+        concat_codes_serial = concat_codes.permute(2, 3, 4, 0, 1)
+        concat_codes_serial = concat_codes_serial.contiguous().view(dim_list1[2], dim_list1[3], dim_list1[4], -1)
+        concat_codes_serial = concat_codes_serial.permute(3, 0, 1, 2)
+        model = RelationNetwork(1024, 512)
+        outputs = RelationNetwork.forward(model, concat_codes_serial)
+        dim_list2 = list(outputs.size())
+        outputs = outputs.view(int(np.sqrt(dim_list2[0])), dim_list2[1], dim_list2[2], dim_list2[3], int(np.sqrt(dim_list2[0])))  #torch.cat((output1, output2), dim = 1)
+        outputs = outputs.permute(0, 4, 1, 2, 3)
+        outputs = torch.mean(outputs, dim=1)
+
+        return outputs
+
+    def average_codes_per_class(self, codes):
+        dim_list1 = list(codes.size())
+        codes = codes.permute(1, 2, 3, 0) #permute to ensure that view is not distructing the tensor structure
+        codes = codes.view(dim_list1[1], dim_list1[2], dim_list1[3], self.config["data_type"]["train"]["num_classes"],
+                           self.config["data_type"]["train"]["n_train_per_class"])
+        codes = codes.permute(3, 4, 0, 1, 2)
+        codes = torch.mean(codes, dim = 1)
+        codes = codes.unsqueeze(1).repeat(1, self.config["data_type"]["train"]["n_train_per_class"], 1, 1, 1)
+        dim_list2 = list(codes.size())
+        codes = codes.permute(2, 3, 4, 0, 1)
+        codes = codes.contiguous().view(dim_list2[2], dim_list2[3], dim_list2[4], -1)
+        codes = codes.permute(3, 0, 1, 2)
+        return codes
+
+    def possibly_sample(self, distribution_params, stddev_offset=0.):
+        dim_list = list(distribution_params.size())
+        means, unnormalized_stddev = torch.split(distribution_params, int(dim_list[1]/2), dim = 1)
+        stddev = torch.exp(unnormalized_stddev)
+        stddev -= (1. - stddev_offset)
+        stddev = torch.max(stddev, torch.tensor(1e-10))
+        distribution = Normal(means, stddev)
+        samples = distribution.sample()
+        kl_divergence = self.kl_divergence(samples, distribution)
+        return samples, kl_divergence
+
+    def kl_divergence(self, samples, normal_distribution):
+        random_prior = Normal(torch.zeros_like(samples), torch.ones_like(samples))
+        kl = torch.mean(normal_distribution.log_prob(samples) - random_prior.log_prob(samples))
+        return kl
 
     def leo_inner_loop(self, data, latents):
         inner_lr = self.config['hyperparameters']['inner_loop_lr']
--- leo_segmentation/run.py
+++ leo_segmentation/run.py
@@ -14,18 +14,28 @@ args = parser.parse_args()
 dataset = args.dataset
 print(dataset)
 
+def get_in_sequence(data, batch_size, H, W):
+    data = data.permute(0, 3, 4, 1, 2)
+    data = data.contiguous().view(batch_size, H, W, -1)
+    data = data.permute(0, 3, 1, 2)
+    return data
 
 def train_model(config):
     metatrain_dataloader = Datagenerator(dataset, config, data_type="train")
     epochs = config["hyperparameters"]["epochs"]
+
     for i in range(epochs):
         tr_data, tr_data_masks, val_data, val_masks = metatrain_dataloader.get_batch_data()
-        tr_data_ = variable(tr_data, requires_grad=True)
         print("tr_data shape: {},tr_data_masks shape: {}, val_data shape: {},val_masks shape: {}". \
               format(tr_data.size(), tr_data_masks.size(), val_data.size(), val_masks.size()))
+        dim_list = list(tr_data.size())
+        tr_data_ = get_in_sequence(tr_data, dim_list[0], dim_list[3], dim_list[4])
+        tr_data_ = tr_data_.unsqueeze(2) #because in the sample_data num_channels is missing
+        #tr_data_ = variable(tr_data_, requires_grad=True)
         model = LEO(config)
         for i, data in enumerate(tr_data_):
-            latents = model.forward_encoder(data)
+            data = data.clone().detach().requires_grad_(True)#torch.tensor(data, requires_grad = True)
+            latents, kl = model.forward_encoder(data)
             tr_loss, adapted_classifier_weights = model.leo_inner_loop(data, latents)
             val_loss, val_accuracy = model.finetuning_inner_loop(data, tr_loss, adapted_classifier_weights)
 
