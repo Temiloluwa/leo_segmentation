9619f221ced93f7fa0cb9961b98f6bf7fe3515f6
Temiloluwa <temilolu74@yahoo.co.uk>
2020-08-22 13:55:51 +0200

    model updated

--- leo_segmentation/model.py
+++ leo_segmentation/model.py
@@ -44,38 +44,39 @@ class LEO(nn.Module):
     """
     contains functions to perform latent embedding optimization
     """
-    def __init__(self, config, mode="meta_train"):
+    def __init__(self, config, weights_shape, mode="meta_train"):
         super(LEO, self).__init__()
         self.config = config
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() and config.use_gpu else "cpu")
         self.mode = mode
         self.loss = nn.CrossEntropyLoss()
-        self.enc1 = _EncoderBlock(14, 32)
-        self.enc2 = _EncoderBlock(32, 32, dropout=True)
-        self.dec2 = _DecoderBlock(32, 32, 32, 32)
-        self.dec1 = _DecoderBlock(32, 32, 32, 14) 
-        self.decoder_loss = torch.nn.MSELoss(reduction="mean")
-        self.seg_net_act = torch.nn.ReLu() 
+        self.enc1 = _EncoderBlock(14, 32).to(self.device) 
+        self.enc2 = _EncoderBlock(32, 32, dropout=True).to(self.device) 
+        self.dec2 = _DecoderBlock(32, 32, 32, 32).to(self.device) 
+        self.dec1 = _DecoderBlock(32, 32, 32, 14).to(self.device)  
+        self.segnet = Segmodel(weights_shape).to(self.device) 
 
     def forward_encoder(self, inputs):
         o = self.enc1(inputs)
         o = self.enc2(o)
         return o
 
-    def forward_decoder(self, inputs, latents, target):
+    def forward_decoder(self, inputs, latents, targets):
         o = self.dec2(latents)
         predicted_weights = self.dec1(o)
-        inner_loss = self.calculate_inner_loss(inputs, predicted_weights, targets)
+        inner_loss = self.calculate_inner_loss(inputs, targets, predicted_weights)
         return inner_loss, predicted_weights
        
 
-    def calculate_inner_loss(self, inputs, weights, targets):
-        num_channels, _, _, _ = weights.shape
-        out_channel1 = torch.sum(inputs * weights[:int(num_channels/2), :, :, :], dim=0)
-        out_channel2 = torch.sum(inputs * weights[int(num_channels/2):, :, :, :], dim=0)
-        output = torch.cat((out_channel1, out_channel2), 0)
-        prediction = torch.argmax(output, dim=0)
-        prediction = self.seg_net_act(prediction)
-        return self.decoder_loss(prediction, targets)
+    def calculate_inner_loss(self, inputs, targets, predicted_weights):
+        self.segnet.channel_zero_weight = nn.Parameter(predicted_weights, requires_grad=True)
+        self.segnet.channel_one_weight = nn.Parameter(predicted_weights, requires_grad=True)
+        pred = self.segnet(inputs).to(self.device)
+        hot_targets = one_hot_target(targets).to(self.device)
+        hot_targets.requires_grad = True
+        pred = softmax(pred)
+        loss = sparse_crossentropy(hot_targets, pred)
+        return loss
         
 
     def leo_inner_loop(self, inputs, latents, target):
@@ -137,3 +138,52 @@ class LEO(nn.Module):
         metatrain_gradients = torch.autograd.grad(metatrain_loss, metatrain_variables, retain_graph=True)
         #nan_loss_or_grad = torch.isnan(metatrain_loss)| torch.reduce([torch.reduce(torch.isnan(g))for g in metatrain_gradients])
         return metatrain_gradients, metatrain_variables
+
+class Segmodel(nn.Module):
+    def __init__(self, weight_shape):
+        super(Segmodel, self).__init__()
+        self.channel_zero_weight = nn.Parameter(torch.normal(torch.zeros(weight_shape), torch.ones(weight_shape)),\
+                                            requires_grad=True)
+        self.channel_one_weight = nn.Parameter(torch.normal(torch.zeros(weight_shape), torch.ones(weight_shape)),\
+                                            requires_grad=True)
+        #self.channel_one_bias = nn.Parameter(torch.normal(torch.zeros(weight_shape), torch.ones(weight_shape)),\
+        #                                   requires_grad=True)
+        #self.channel_two_bias = nn.Parameter(torch.normal(torch.zeros(weight_shape), torch.ones(weight_shape)),\
+        #                                    requires_grad=True)
+        #self.scale = nn.Parameter(torch.tensor(10.0, requires_grad=True))
+        self.channel_one_bias = 1.0
+        self.channel_two_bias = 1.0
+        self.scale = 5.0
+
+
+    def forward(self, x):
+        channel_zero = x * self.channel_zero_weight/self.scale + self.channel_one_bias
+        channel_zero = torch.unsqueeze(torch.sum(channel_zero, dim=1), 1)
+        channel_one = x * self.channel_one_weight/self.scale + self.channel_two_bias
+        channel_one = torch.unsqueeze(torch.sum(channel_one, dim=1), 1)
+        return torch.cat([channel_zero, channel_one], 1)
+
+def calc_iou_per_class(pred_x, targets):
+    iou_per_class = []
+    for i in range(len(pred_x)):
+        pred = np.argmax(pred_x[i].cpu().detach().numpy(), 0).astype(int)
+        target = targets[i].astype(int)
+        iou = np.sum(np.logical_and(target, pred))/np.sum(np.logical_or(target, pred))
+        iou_per_class.append(iou)
+        mean_iou_per_class = np.mean(iou_per_class)
+    return mean_iou_per_class
+
+def one_hot_target(mask, channel_dim=1):
+    mask_inv = (~torch.tensor([1,0,1,0]).type(torch.bool)).type(torch.float32)
+    channel_zero = torch.unsqueeze(mask_inv, channel_dim)
+    channel_one = torch.unsqueeze(mask, channel_dim)
+    return torch.cat((channel_zero, channel_one), axis=channel_dim)
+
+def softmax(py_tensor, channel_dim=1):
+    py_tensor = torch.exp(py_tensor)
+    return  py_tensor/torch.unsqueeze(torch.sum(py_tensor, dim=channel_dim), channel_dim)
+
+def sparse_crossentropy(target, pred,  channel_dim=1, eps=1e-10):
+    pred += eps
+    loss = torch.sum(-1 * target * torch.log(pred), dim=channel_dim)
+    return torch.mean(loss)
