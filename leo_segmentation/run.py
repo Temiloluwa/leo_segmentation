75cb89f3ecf8ed800d95e64d966ace9b60ec7063
Temiloluwa <temilolu74@yahoo.co.uk>
2020-09-23 15:57:42 +0200

    pep8 on run.py

--- leo_segmentation/run.py
+++ leo_segmentation/run.py
@@ -1,24 +1,29 @@
-import sys, os, argparse, time, numpy as np, tensorflow as tf
+import sys
+import os
+import argparse
+import time
+import numpy as np
+import tensorflow as tf
+from easydict import EasyDict as edict
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from leo_segmentation.data import Datagenerator, TrainingStats
 from leo_segmentation.model import LEO, load_model, save_model
-from leo_segmentation.utils import load_config, check_experiment, get_named_dict, \
-                        log_data, load_yaml, train_logger, val_logger, print_to_string_io, \
-                        save_pickled_data, model_dir
-from easydict import EasyDict as edict
-from IPython import get_ipython
+from leo_segmentation.utils import load_config, check_experiment,\
+    get_named_dict, log_data, load_yaml, train_logger, val_logger, \
+    print_to_string_io, save_pickled_data, model_dir
 
 try:
     shell = get_ipython().__class__.__name__
     if shell == "NoneType":
         raise NameError("Move to except branch")
 except NameError:
-    parser = argparse.ArgumentParser(description='Specify train or inference dataset')
+    parser = argparse.ArgumentParser(description='Specify dataset')
     parser.add_argument("-d", "--dataset", type=str, default="pascal_voc_raw")
     args = parser.parse_args()
     dataset = args.dataset
 
-#TO-DO change to tensorflow
+
+# TO-DO change to tensorflow
 def load_model_and_params(config):
     """Loads model and accompanying saved parameters"""
     leo, optimizer, stats = load_model(config)
@@ -29,6 +34,7 @@ def load_model_and_params(config):
     train_stats.set_episode(episodes_completed)
     train_stats.update_stats(**stats)
     return leo, optimizer, train_stats
+
 
 def train_model(config, dataset):
     """Trains Model"""
@@ -51,31 +57,34 @@ def train_model(config, dataset):
         dataloader = Datagenerator(config, dataset, data_type="meta_train")
         img_transformer = dataloader.transform_image
         mask_transformer = dataloader.transform_mask
-        transformers = (img_transformer, mask_transformer) 
+        transformers = (img_transformer, mask_transformer)
         metadata = dataloader.get_batch_data()
         _, train_stats = leo.compute_loss(metadata, train_stats, transformers)
 
         if episode % config.checkpoint_interval == 0:
-            #save model
+            # save model
             pass
-    
         if episode % config.meta_val_interval == 0:
             dataloader = Datagenerator(config, dataset, data_type="meta_val")
             train_stats.set_mode("meta_val")
             metadata = dataloader.get_batch_data()
-            _, train_stats = leo.compute_loss(metadata, train_stats, transformers, mode="meta_val")
+            _, train_stats = leo.compute_loss(metadata, train_stats,
+                                              transformers, mode="meta_val")
             train_stats.disp_stats()
         episode_time = (time.time() - start_time)/60
-        log_msg = print_to_string_io(f"Episode: {episode}, Episode Time: {episode_time:0.03f} minutes", False)
+        log_msg = print_to_string_io(f"Episode: {episode}, Episode Time:\
+            {episode_time:0.03f} minutes", False)
         train_logger.debug(log_msg)
         episode_times.append(episode_time)
 
-    model_and_params = leo, None, train_stats 
+    model_and_params = leo, None, train_stats
     leo = predict_model(config, dataset, model_and_params, transformers)
-    log_msg = print_to_string_io(f"Total Model Training Time {np.sum(episode_times):0.03f} minutes", False)
+    log_msg = print_to_string_io(f"Total Model Training Time \
+        {np.sum(episode_times):0.03f} minutes", False)
     train_logger.debug(log_msg)
     train_logger.debug(f"End time")
     return leo
+
 
 def predict_model(config, dataset, model_and_params, transformers):
     """Implement Predicion on Meta-Test"""
@@ -83,18 +92,23 @@ def predict_model(config, dataset, model_and_params, transformers):
     dataloader = Datagenerator(config, dataset, data_type="meta_test")
     train_stats.set_mode("meta_test")
     metadata = dataloader.get_batch_data()
-    _, train_stats = leo.compute_loss(metadata, train_stats, transformers, mode="meta_test")
+    _, train_stats = leo.compute_loss(metadata, train_stats, transformers,
+                                      mode="meta_test")
     train_stats.disp_stats()
     experiment = config.experiment
     for mode in ["meta_train", "meta_val", "meta_test"]:
         stats_df = train_stats.get_stats(mode)
         ious_df = train_stats.get_ious(mode)
-        stats_df.to_pickle(os.path.join(model_dir, f"experiment_{mode}_{experiment.number}_stats.pkl"))
-        ious_df.to_pickle(os.path.join(model_dir, f"experiment_{mode}_{experiment.number}_ious.pkl"))
-    log_data("************** Hyperparameters Used ************\n", os.path.join(model_dir, "train_log.txt"))
+        stats_df.to_pickle(os.path.join(model_dir,
+                           f"experiment_{mode}_{experiment.number}_stats.pkl"))
+        ious_df.to_pickle(os.path.join(model_dir,
+                          f"experiment_{mode}_{experiment.number}_ious.pkl"))
+    log_data("************** Hyperparameters Used ************\n",
+             os.path.join(model_dir, "train_log.txt"))
     msg = print_to_string_io(config.hyperparameters, True)
     log_data(msg, os.path.join(model_dir, "train_log.txt"))
     return leo
+
 
 def main():
     config = load_config()
@@ -105,10 +119,12 @@ def main():
             dataloader = Datagenerator(config, dataset, data_type="meta_train")
             img_transformer = dataloader.transform_image
             mask_transformer = dataloader.transform_mask
-            transformers = (img_transformer, mask_transformer) 
+            transformers = (img_transformer, mask_transformer)
             model_and_params = load_model_and_params(config)
-            return predict_model(config, dataset, model_and_params, transformers)
+            return predict_model(config, dataset, model_and_params,
+                                 transformers)
         evaluate_model()
-    
+
+
 if __name__ == "__main__":
     main()
