5f5159bf91bce42e7bc5a41dc0e38eae4a5ef497
Temiloluwa <temilolu74@yahoo.co.uk>
2020-11-01 12:34:31 +0100

    memory growth tf

--- run.py
+++ run.py
@@ -23,6 +23,15 @@ except NameError:
     dataset = args.dataset
 
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    print(e)
+
+
 def load_model_and_params():
     """Loads model and accompanying saved parameters"""
     leo, optimizer, stats = load_model()
